b563f8954210ea26cbd53b0adf6354cbeff4e7c8
Megha Verma <mverma1427@gmail.com>
2025-07-20 20:09:47 +0530

    Complete Roman class implementation and add relevant tests

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -1,37 +1,69 @@
 package tudelft.roman;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RomanNumeral {
 
-    private static Map<Character, Integer> map;
+    private static final Map<Character, Integer> symbToNumMap;
+    private static final Map<Integer, String> numToSymbMap;
 
     static {
-        map = new HashMap<Character, Integer>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+        symbToNumMap = new HashMap<>();
+        symbToNumMap.put('I', 1);
+        symbToNumMap.put('V', 5);
+        symbToNumMap.put('X', 10);
+        symbToNumMap.put('L', 50);
+        symbToNumMap.put('C', 100);
+        symbToNumMap.put('D', 500);
+        symbToNumMap.put('M', 1000);
+        
+        numToSymbMap = new TreeMap<>(Collections.reverseOrder());
+        numToSymbMap.put(1, "I");
+        numToSymbMap.put(4, "IV");
+        numToSymbMap.put(5, "V");
+        numToSymbMap.put(9, "IX");
+        numToSymbMap.put(10, "X");
+        numToSymbMap.put(40, "XL");
+        numToSymbMap.put(50, "L");
+        numToSymbMap.put(90, "XC");
+        numToSymbMap.put(100, "C");
+        numToSymbMap.put(400, "CD");
+        numToSymbMap.put(500, "D");
+        numToSymbMap.put(900, "CM");
+        numToSymbMap.put(1000, "M");
     }
 
     public int convert(String s) {
-
         int convertedNumber = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int currentNumber = map.get(s.charAt(i));
-            int next = i+1 < s.length() ? map.get(s.charAt(i+1)) : 0;
+        for (int i = 0; i < s.length(); i++) {
+            int curr = symbToNumMap.get(s.charAt(i));
+            int next = i + 1 < s.length() ? symbToNumMap.get(s.charAt(i + 1)) : 0;
 
-            if(currentNumber >= next)
-                convertedNumber += currentNumber;
-            else
-                convertedNumber -= currentNumber;
+            // Usually, you add symbol values left to right.
+            // BUT: if a smaller numeral comes before a larger
+            // one, you subtract it instead. Examples:
+            // VI = 5 + 1 = 6
+            // IV = 5 - 1 = 4
+            // IX = 10 - 1 = 9
+
+            // So, you look ahead just to decide if current is
+            // + or -. The next is still added on its own turn.
+            convertedNumber += (next <= curr) ? curr : -curr;
         }
-
         return convertedNumber;
+    }
 
+    public String convert(int num) {
+        StringBuilder convertedStr = new StringBuilder();
+
+        while (num > 0) {
+            for (Integer val : numToSymbMap.keySet()) {
+                if (val > num) continue;
+                convertedStr.append(numToSymbMap.get(val));
+                num -= val;
+                break;
+            }
+        }
+        return convertedStr.toString();
     }
 }
--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
-
     @Test
     public void singleNumber() {
         RomanNumeral roman = new RomanNumeral();
@@ -32,5 +31,47 @@ public class RomanNumeralTest {
         RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
+    }
+
+    @Test
+    public void singleSymbol() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(1);
+        Assertions.assertEquals("I", result);
+    }
+
+    @Test
+    public void symbolWithManyChars() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(8);
+        Assertions.assertEquals("VIII", result);
+    }
+
+    @Test
+    public void symbolWithSubtractiveNotation() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(4);
+        Assertions.assertEquals("IV", result);
+    }
+
+    @Test
+    public void symbolWithAndWithoutSubtractiveNotation() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(44);
+        Assertions.assertEquals("XLIV", result);
+    }
+
+    @Test
+    public void testWith5000() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(5000);
+        Assertions.assertEquals("MMMMM", result);
+    }
+
+    @Test
+    public void testWith6000() {
+        RomanNumeral roman = new RomanNumeral();
+        String result = roman.convert(6000);
+        Assertions.assertEquals("MMMMMM", result);
     }
 }
